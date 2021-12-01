9b4ad2b8e3c86f3a0d21d1fc1891af2e84d5e0f2
Upendra Sahu <upendra.sahu@maplelabs.com>
2021-12-01 10:35:20 +0530

    steps

---
+++ ror_lambda/aws-config.sh
@@ -0,0 +1,5 @@
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=us-west-2
+export RAILS_ENV=${RAILS_ENV:=production}
+
---
+++ ror_lambda/steps.sh
@@ -0,0 +1,4 @@
+source ./aws-config.sh
+./bin/setup
+./bin/build
+./bin/_deploy_upen
